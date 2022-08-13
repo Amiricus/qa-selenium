55af4106a2fd1cdaa7d0614ec598298f9fad3db4
Амир Байтулинов <amiricus@gmail.com>
2022-08-13 13:38:40 +0500

    add chromedriver.setup

--- src/test/java/ru/netology/qa/selenium/OrderingcardTest.java
+++ src/test/java/ru/netology/qa/selenium/OrderingcardTest.java
@@ -1,5 +1,6 @@
 package ru.netology.qa.selenium;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +16,7 @@ class OrderingcardTest {
     @BeforeAll
     static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "./drivers/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
